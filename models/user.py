4cf2490dec3c9109e6754416291a5a9d8338b569
Ronald Abu Saleh <ronald.abusaleh@gmail.com>
2024-09-13 09:46:36 -0400

    change in User model

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.sql import func
 from database.session import Base
 
@@ -7,7 +7,7 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(127), index=True, nullable=False)
-    email = Column(String(127), unique=True, index=True, nullable=False)
+    name = Column(String(127), nullable=False)
+    email = Column(String(127), unique=True, nullable=False)
     password = Column(String(127), nullable=False)
-    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(TIMESTAMP, server_default=func.now())
